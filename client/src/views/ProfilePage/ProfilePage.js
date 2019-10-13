0f63a24d387f26e453ad68ba9ed4724831e83bed
Eric Lee <Raidenlkl@gmail.com>
2019-10-13 10:52:54 +1000

    Working on material ui pro folder

---
+++ client/src/views/ProfilePage/ProfilePage.js
@@ -0,0 +1,100 @@
+/*eslint-disable*/
+import React from 'react';
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+// @material-ui/icons
+
+import Favorite from '@material-ui/icons/Favorite';
+// core components
+import Header from 'components/Header/Header.js';
+import Footer from 'components/Footer/Footer.js';
+import HeaderLinks from 'components/Header/HeaderLinks.js';
+import Parallax from 'components/Parallax/Parallax.js';
+import Hero from './Hero';
+
+import profilePageStyle from 'assets/jss/material-kit-pro-react/views/profilePageStyle.js';
+
+const useStyles = makeStyles(profilePageStyle);
+
+export default function ProfilePage({ ...rest }) {
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+  });
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Header
+        color='transparent'
+        brand='Eric lee'
+        links={<HeaderLinks dropdownHoverColor='info' />}
+        fixed
+        changeColorOnScroll={{
+          height: 200,
+          color: 'info'
+        }}
+        {...rest}
+      />
+      <Hero />
+
+      <Footer
+        content={
+          <div>
+            <div className={classes.left}>
+              <List className={classes.list}>
+                <ListItem className={classes.inlineBlock}>
+                  <a
+                    href='https://www.creative-tim.com/?ref=mkpr-profile'
+                    target='_blank'
+                    className={classes.block}
+                  >
+                    Creative Tim
+                  </a>
+                </ListItem>
+                <ListItem className={classes.inlineBlock}>
+                  <a
+                    href='https://www.creative-tim.com/presentation?ref=mkpr-profile'
+                    target='_blank'
+                    className={classes.block}
+                  >
+                    About us
+                  </a>
+                </ListItem>
+                <ListItem className={classes.inlineBlock}>
+                  <a href='//blog.creative-tim.com/' className={classes.block}>
+                    Blog
+                  </a>
+                </ListItem>
+                <ListItem className={classes.inlineBlock}>
+                  <a
+                    href='https://www.creative-tim.com/license?ref=mkpr-profile'
+                    target='_blank'
+                    className={classes.block}
+                  >
+                    Licenses
+                  </a>
+                </ListItem>
+              </List>
+            </div>
+            <div className={classes.right}>
+              &copy; {1900 + new Date().getYear()} , made with{' '}
+              <Favorite className={classes.icon} /> by{' '}
+              <a
+                href='https://www.creative-tim.com?ref=mkpr-profile'
+                target='_blank'
+              >
+                Creative Tim
+              </a>{' '}
+              for a better web.
+            </div>
+          </div>
+        }
+      />
+    </div>
+  );
+}
