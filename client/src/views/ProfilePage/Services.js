c7d85c65271abcdcf79d9168ed9ffc4de3704817
Eric Lee <Raidenlkl@gmail.com>
2019-10-13 15:34:58 +1000

    Service card

--- client/src/views/ProfilePage/Services.js
+++ client/src/views/ProfilePage/Services.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import { Typography, Card, Grid, Container } from '@material-ui/core';
+import WebIcon from '@material-ui/icons/Web';
 
 const Services = () => {
   return (
-    <section style={{ height: '90vh' }} className='services'>
+    <section className='services'>
       <h3>My Services</h3>
       <Container>
         <Grid container spacing={3} justify='center' alignItems='center'>
           <Grid xs={12} md={4} item>
-            <Card style={{ height: '500px' }}>
-              <h3>Front End</h3>
+            <Card className='services_cards'>
+              <i className='services_cards_icons fas fa-laptop'></i>
+              <h3>Web Development</h3>
+              <h4>Responsive / Interactive</h4>
+              <p>
+                Every website should be built with two primary goals: Firstly,
+                it needs to work across all devices. Secondly, it needs to be
+                fast as possible.
+              </p>
             </Card>
           </Grid>
           <Grid xs={12} md={4} item>
-            <Card style={{ height: '500px' }}>
-              <h3>Back End</h3>
+            <Card className='services_cards'>
+              <i className='services_cards_icons fas fa-server'></i>
+              <h3>Server Apps Development</h3>
+              <h4>Robust / Scalable </h4>
+              <p>
+                Every website should be built with two primary goals: Firstly,
+                it needs to work across all devices. Secondly, it needs to be
+                fast as possible.
+              </p>
             </Card>
           </Grid>
           <Grid xs={12} md={4} item>
-            <Card style={{ height: '500px' }}>
-              <h3>Mobile Apps</h3>
+            <Card className='services_cards'>
+              <i className='services_cards_icons fas fa-mobile-alt'></i>
+              <h3>Mobile Apps Development</h3>
+              <h4>Portable / User Friendly </h4>
+              <p>
+                Every website should be built with two primary goals: Firstly,
+                it needs to work across all devices. Secondly, it needs to be
+                fast as possible.
+              </p>
             </Card>
           </Grid>
         </Grid>
