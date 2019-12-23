0bc90ba70aaf5a24706535ca795800824c47db8e
Eric Lee <Raidenlkl@gmail.com>
2019-12-23 12:40:04 +1000

    Footer remake

--- clientts/src/components/Layout/Footer.tsx
+++ clientts/src/components/Layout/Footer.tsx
@@ -1,25 +1,44 @@
 import React, { Fragment } from 'react';
-
+import { Fab } from '@material-ui/core';
 const Footer: React.FC = () => {
   return (
     <Fragment>
       <div className='footer'>
-        <h2 className='footer_title bold'>Contact Me</h2>
+        <div className='footer_content'>
+          <div className='footer_content_contactbox'>
+            <h2 className='footer_content_contactbox_title bold'>
+              Let's Work Together
+            </h2>
+            <h3 className='footer_content_contactbox_subtitle'>
+              Have a project you'd like to discuss?
+            </h3>
+            <Fab
+              style={{ margin: '1.5rem auto' }}
+              variant='extended'
+              color='primary'
+              aria-label='add'
+            >
+              Contact Me!
+            </Fab>
+          </div>
 
-        <div className='footer_socials'>
-          <i className='fab fa-facebook-square fa-3x' />
-          <i className='fab fa-instagram fa-3x' />
-          <i className='fab fa-linkedin fa-3x' />
-          <i className='fab fa-github-square fa-3x' />
+          <div className='footer_content_methods'>
+            <div className='footer_content_methods_method'>
+              <i className='far fa-envelope' /> Ericlkl.au@gmail.com
+            </div>
+            <div className='footer_content_methods_method'>
+              <i className='fab fa-facebook-square' /> On Facebook
+            </div>
+            <div className='footer_content_methods_method'>
+              <i className='fab fa-linkedin' /> On linkedin
+            </div>
+            <div className='footer_content_methods_method'>
+              <i className='fab fa-instagram' /> On Instagram
+            </div>
+          </div>
         </div>
 
         <div className='footer_copyright'>
-          <div className='footer_copyright_links'>
-            <h4>Eric lee</h4>
-            <h4>About</h4>
-            <h4>Projects</h4>
-            <h4>Contact</h4>
-          </div>
           <h4>Developed By Eric LEE &copy; All Right Reserved</h4>
         </div>
       </div>
