c51d5203dfdb12bc4becf11ba9b0cbdd8b088135
Eric Lee <Raidenlkl@gmail.com>
2021-06-12 17:51:24 +0800

    refactor: 404page setup

---
+++ server/pages/404.tsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+import Navbar from '../components/Layout/Navbar';
+import Footer from '../components/Layout/Footer';
+// Page Components
+import PageComponents from '../sections/404';
+const { Content } = PageComponents;
+
+const ErrorPage: React.FC = () => {
+  return (
+    <Fragment>
+      <Navbar logoColor="blue" />
+      <Content />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default ErrorPage;
---
+++ server/sections/404/Content.tsx
@@ -0,0 +1,17 @@
+import styles from '../../styles/pages/404/content.module.scss';
+
+const Content = () => {
+  return (
+    <section className={styles.notfound}>
+      <div className={`${styles.notfound_content} container`}>
+        <h4 className={styles.notfound_content_oops}>OOPS! Page Not Found</h4>
+        <h1 className={styles.notfound_content_404}>404</h1>
+        <h4 className={styles.notfound_content_sorry}>
+          THE PAGE YOU REQUESTED WAS NOT FOUND!
+        </h4>
+      </div>
+    </section>
+  );
+};
+
+export default Content;
